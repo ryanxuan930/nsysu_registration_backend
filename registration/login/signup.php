407c0f591c198ad75306fb645dddf60caa0a47f0
ryanxuan930 <anakinchung930@gmail.com>
2021-10-09 00:13:42 +0800

    Update signup.php

--- registration/login/signup.php
+++ registration/login/signup.php
@@ -4,7 +4,7 @@ header("Access-Control-Allow-Methods: *");
 header("Access-Control-Allow-Headers: Origin, Methods, Content-Type");
 require("../../database.php");
 if(isset($_POST["dept"]) and strlen($_POST["dept"])==3){
-    $org = $_POST["dept"];
+    $dept = $_POST["dept"];
 }else{
     echo "dept";
     exit();
