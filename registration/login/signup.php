1fe282fe76256cb4834c8665eba69cfa36170d1e
ryanxuan930 <anakinchung930@gmail.com>
2021-10-10 22:51:05 +0800

    Update signup.php

--- registration/login/signup.php
+++ registration/login/signup.php
@@ -4,43 +4,43 @@ require("../../database.php");
 if(isset($_POST["dept"])){
     $dept = $_POST["dept"];
 }else{
-    echo "dept";
+    echo "invalid_dept";
     exit();
 }
 if(isset($_POST["email"]) and filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
     $email = $_POST["email"];
 }else{
-    echo "email";
+    echo "invalid_email";
     exit();
 }
 if(isset($_POST["stu_id"]) and strlen($_POST["stu_id"])==10){
     $stu_id= $_POST["stu_id"];
 }else{
-    echo "account";
+    echo "invalid_stu_id";
     exit();
 }
 if(isset($_POST["g_year"]) and is_numeric($_POST["g_year"])){
     $g_year = $_POST["g_year"];
 }else{
-    echo "number";
+    echo "invalid_g_year";
     exit();
 }
 if(isset($_POST["name"])){
     $name = htmlspecialchars($_POST["name"]);
 }else{
-    echo "name";
+    echo "invalid_name";
     exit();
 }
 if(isset($_POST["sex"]) and ($_POST["sex"]==1 or $_POST["sex"]==2)){
     $sex = $_POST["sex"];
 }else{
-    echo "sex";
+    echo "invalid_sex";
     exit();
 }
 if(isset($_POST["password"])){
     $password = password_hash($_POST["password"],PASSWORD_DEFAULT);
 }else{
-    echo "password";
+    echo "invalid_password";
     exit();
 }
 $conn->select_db('sep3');
